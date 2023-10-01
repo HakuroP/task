af6ca6c5c4cccce2a2ab28583ad08f40aa0b3b16
Your Name <hakurop07@gmail.com>
2023-10-01 09:30:06 +0700

    first commit

---
+++ src/Lesson1/Excercise1.java
@@ -0,0 +1,14 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class Excercise1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int h = s / 3600;
+        int m = s % 3600 / 60;
+        int sec = s % 3600 % 60;
+        System.out.println(h + "h " + m + "m " + sec + "sec");
+    }
+}
